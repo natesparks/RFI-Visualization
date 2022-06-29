f203a33c729f2548d1841b6bb8206eab4607105c
Nathan Sparks <nsparks@usc.edu>
2022-06-29 15:04:10 -0400

    rmsPlotterHMS.py now uses rfiTableHandler to calculate rms for each scan

--- rmsPlotterHMS.py
+++ rmsPlotterHMS.py
@@ -6,8 +6,9 @@ from math import sqrt
 import numpy as np
 import datetime
 import timestampHandler
+from rfiTableHandler import rfiTableHandler
     
-def calculateScanRMS(source_directory, freq_min, freq_max) :
+def calcRMSoverTime(source_directory, freq_min, freq_max) :
     # list of all filepaths within directory
     filepathlist = (glob.glob(source_directory + "/*.txt"))
 
@@ -16,66 +17,10 @@ def calculateScanRMS(source_directory, freq_min, freq_max) :
     rmsList = []
     for filepath in filepathlist :
         # variable declarations
-        rms = 0.0
-        frequency = 0.0
-        intensity = 0.0
-        intensityList = []
-        NaNcount = 0
-
-        with open(filepath, 'r') as ifile :
-
-            # read 21 header lines before data appears
-            for i in range(0,21) :
-                line = ifile.readline()
-                # parse date
-                if (i == 2) :
-                    scanDate = line.split()[2].split('-')
-                    scanYear = int(scanDate[0])
-                    scanMonth = int(scanDate[1])
-                    scanDay = int(scanDate[2])
-                # parse time
-                if (i == 3) : 
-                    scanHour = float((line.split())[3])
-                    scanMinute = 60 * (scanHour % 1) 
-                    scanSecond = 60 * (scanMinute % 1) 
-            scanDatetime = datetime.datetime(scanYear, scanMonth, scanDay, int(scanHour), int(scanMinute), int(scanSecond)) 
-
-            # Pprse frequency and intensity data from file
-            while (1) :
-                # take in the data line by line
-                line  = ifile.readline()
-                data = line.split()
-
-                # EOF condition
-                length = len(data)
-                if (length < 4) : break
-
-                # extract frequency and intensity
-                frequency = float(data[2])
-                intensity = float(data[3])
-
-                # skip points less than frequency min
-                if (frequency < freq_min) : continue
-                # stop reading if frequency max is exceeded
-                if (frequency > freq_max) : break
-                # skip points with invalid intensity (NaN)
-                if (data[3] == "NaN") :
-                    NaNcount += 1
-                    continue
-                # add intensity to list 
-                intensityList.append(intensity)
-
-            # print("Reading: " + filepath)
-
-
-        # calculate rms
-        if (intensityList) :
-            rms = np.square(intensityList).mean()
-            rms = sqrt(rms)  
-        else : #empty intensityList (no frequencies in this scan are within [freq_min, freq_max])
-            rms = float("nan")
+        curr_rfiTableHandler = rfiTableHandler(filepath)
+        rms = curr_rfiTableHandler.calcRMSsingleChannel(freq_min, freq_max)
         rmsList.append(rms)
-        timeList.append(scanDatetime)
+        timeList.append(curr_rfiTableHandler.scanDatetime)
     # print(f"{freq_min} {freq_max} has rms list:")
     # print(rmsList)
     return (timeList, rmsList)
@@ -104,7 +49,6 @@ def plotRMS(timeList, rmsList, freq_min, freq_max, timestamp_filepath) :
         print("Reading: " + timestamp_filepath)
         # read timestamps
         eventnames, eventdatetimes = timestampHandler.parsefile(timestamp_filepath)
-
         # shade background sections corresponding to timestamps
         labellist = []
         eventColorMap = {'All_on' : 'silver', 'Xband_off' : 'turquoise', 'Kuband_off' : 'blue', 'Fat_fingering' : 'crimson'}
@@ -123,7 +67,6 @@ def plotRMS(timeList, rmsList, freq_min, freq_max, timestamp_filepath) :
                 plt.axvspan(eventtime, eventdatetimes[i+1], facecolor = eventColorMap[eventname], alpha=0.5, label = label, zorder=-100)
         plt.legend(bbox_to_anchor = (1.0, 1.0), loc = 'upper left')
         
-
     plt.tight_layout() # prevent legend from getting cut off
     plt.show()
 
@@ -156,7 +99,7 @@ def main() :
     freq_max = float(sys.argv[3])
 
 
-    timeList, rmsList = calculateScanRMS(source_directory, freq_min, freq_max)
+    timeList, rmsList = calcRMSoverTime(source_directory, freq_min, freq_max)
     plotRMS(timeList, rmsList, freq_min, freq_max, event_timestamps)
 
 
