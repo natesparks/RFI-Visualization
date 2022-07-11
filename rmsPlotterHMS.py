6651298d6b28931fa7cb933eed9c58743ab8f973
Nathan Sparks <nsparks@usc.edu>
2022-07-11 15:57:38 -0400

    prepped rmsPlotterHMS for packaging

--- rmsPlotterHMS.py
+++ rmsPlotterHMS.py
@@ -7,10 +7,23 @@ import numpy as np
 import datetime
 import timestampHandler
 from rfiTableHandler import rfiTableHandler
+
     
-def calcRMSoverTime(source_directory, freq_min, freq_max) :
+def plotRMS(sourceDirectory, freq_min, freq_max, timestampFilepath="") :
+    # check that source directory is valid
+    sourceDirectory = sys.argv[1]
+    if (os.path.isdir(sourceDirectory) is False) :
+        sys.exit("Could not find source directory with name: " + sourceDirectory)
+
+    # check that timestamp filepath is valid, if one exists
+    if (timestampFilepath != "") :
+        if (os.path.exists(timestampFilepath) is False) :
+            sys.exit("Could not find file with path: " + timestampFilepath)
+        if (timestampFilepath.endswith(".txt") is False) :
+            sys.exit("The provided file is not a .txt file: " + timestampFilepath)
+
     # list of all filepaths within directory
-    filepathlist = (glob.glob(source_directory + "/*.txt"))
+    filepathlist = (glob.glob(sourceDirectory + "/*.txt"))
 
     # go through each scan to get the time and rms for a given frequency range
     timeList = []
@@ -21,19 +34,13 @@ def calcRMSoverTime(source_directory, freq_min, freq_max) :
         rms = curr_rfiTableHandler.calcRMSsingleChannel(freq_min, freq_max)
         rmsList.append(rms)
         timeList.append(curr_rfiTableHandler.scanDatetime)
-    # print(f"{freq_min} {freq_max} has rms list:")
-    # print(rmsList)
-    return (timeList, rmsList)
 
-
-    
-def plotRMS(timeList, rmsList, freq_min, freq_max, timestamp_filepath) :
     # start and end time for formatting purposes
     starttime = min(timeList)
     endtime = max(timeList)
 
-    # format axes and text
-    currentFigure = plt.figure(figsize=(10,6))
+    # graph formatting
+    currentFigure = plt.figure(figsize=(16,5))
     plt.xlabel("Time (UTC Hours)")
     plt.ylabel("RMS (Jy)")
     title = f"RMS over time [{starttime.strftime('%H:%M:%S')}, {endtime.strftime('%H:%M:%S')}] in range [{freq_min}, {freq_max}] (GHz)"
@@ -41,21 +48,17 @@ def plotRMS(timeList, rmsList, freq_min, freq_max, timestamp_filepath) :
     plt.rcParams["date.autoformatter.minute"] = "%H:%M:%S"
     plt.xlim(starttime, endtime) # Comment out to add padding         
 
-    # plot rms
-    plt.plot(timeList, rmsList, color='black', linewidth=1.0)
-
-    # display events on graph, if any exist
-    if (timestamp_filepath != "") :
-        print("Reading: " + timestamp_filepath)
+    # shade background sections corresponding to timestamps, if any exist
+    if (timestampFilepath != "") :
         # read timestamps
-        eventnames, eventdatetimes = timestampHandler.parsefile(timestamp_filepath)
-        # shade background sections corresponding to timestamps
-        labellist = []
+        print("Reading: " + timestampFilepath)
+        eventnames, eventdatetimes = timestampHandler.parsefile(timestampFilepath)
         eventColorMap = {'All_on' : 'silver', 'Xband_off' : 'turquoise', 'Kuband_off' : 'blue', 'Fat_fingering' : 'crimson'}
+        labellist = []
         lastEventTime = max(eventdatetimes)
-        for i, data in enumerate(zip(eventnames, eventdatetimes)) :
-            eventname, eventtime = data
-            # label section if color not already labelled
+
+        # plot each event and give it a label
+        for i, (eventname, eventtime) in enumerate(zip(eventnames, eventdatetimes)) :
             label = ""
             if (eventname not in labellist) :
                 labellist.append(eventname)
@@ -65,46 +68,31 @@ def plotRMS(timeList, rmsList, freq_min, freq_max, timestamp_filepath) :
                 plt.axvspan(eventtime, eventtime + datetime.timedelta(minutes=5), facecolor = eventColorMap[eventname], alpha=0.5, label = label, zorder=-100)
             else :
                 plt.axvspan(eventtime, eventdatetimes[i+1], facecolor = eventColorMap[eventname], alpha=0.5, label = label, zorder=-100)
+
+        # legend
         plt.legend(bbox_to_anchor = (1.0, 1.0), loc = 'upper left')
         
-    plt.tight_layout() # prevent legend from getting cut off
+    # plot rms over time
+    plt.plot(timeList, rmsList, color='black', linewidth=1.0)
+
+    # display plot
+    plt.tight_layout() # prevent legend from getting cut off    
     plt.show()
 
 
-def main() :
+
+if __name__ == '__main__':
     # check for number of arguments
     numargs = len(sys.argv)
     if (numargs < 4 or numargs > 5) :
-        sys.exit("Usage: python3 rmsPlotterHMS.py <source_directory> <freq_min> <freq_max> [event_timestamps]")
-
-    # check that source directory is valid
-    source_directory = sys.argv[1]
-    if (os.path.isdir(source_directory) is False) :
-        sys.exit("Could not find source directory with name: " + source_directory)
-
-    # check that timestamp filepath is valid
+        sys.exit("Usage: python3 rmsPlotterHMS.py <sourceDirectory> <freq_min> <freq_max> [event_timestamps]")
+        
+    # check for timestamp file
+    timestampFilepath = ""
     if (numargs == 5) :
-        timestamp_filepath = sys.argv[4]
-        if (os.path.exists(timestamp_filepath) is False) :
-            sys.exit("Could not find file with path: " + timestamp_filepath)
-        if (timestamp_filepath.endswith(".txt") is False) :
-            sys.exit("The provided file is not a .txt file: " + timestamp_filepath)
-        event_timestamps = sys.argv[4]
-    # no timestamp file provided
-    else :
-        event_timestamps=  ""
+        timestampFilepath = sys.argv[4]
 
-    # parse frequency range args
-    freq_min = float(sys.argv[2])
-    freq_max = float(sys.argv[3])
-
-
-    timeList, rmsList = calcRMSoverTime(source_directory, freq_min, freq_max)
-    plotRMS(timeList, rmsList, freq_min, freq_max, event_timestamps)
-
-
-if __name__ == '__main__':
-    main()
+    plotRMS(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), timestampFilepath)
 
 
 
